d294f8b57b47325290a319df8e7b94577230704b
cevans <CEVANS26@gru.edu>
2016-02-20 20:48:32 -0500

    Added SoftDocForm -Brent

---
+++ SoftwareConfigurationManagementDBApp/SoftwareConfigurationManagementDBApp/SoftwareDocForm.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace SoftwareConfigurationManagementDBApp
+{
+    partial class SoftwareDocForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSoftDocName = new System.Windows.Forms.Label();
+            this.txtSoftDocName = new System.Windows.Forms.TextBox();
+            this.lblSoftDocDate = new System.Windows.Forms.Label();
+            this.txtSoftDocDate = new System.Windows.Forms.TextBox();
+            this.lblSoftDocRevision = new System.Windows.Forms.Label();
+            this.txtSoftDocRevision = new System.Windows.Forms.TextBox();
+            this.lblSoftDocLocation = new System.Windows.Forms.Label();
+            this.txtSoftDocLocation = new System.Windows.Forms.TextBox();
+            this.lblSoftDocDescription = new System.Windows.Forms.Label();
+            this.txtSoftDocDescription = new System.Windows.Forms.TextBox();
+            this.lblSoftDoc = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSoftDocName
+            // 
+            this.lblSoftDocName.AutoSize = true;
+            this.lblSoftDocName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftDocName.Location = new System.Drawing.Point(49, 82);
+            this.lblSoftDocName.Name = "lblSoftDocName";
+            this.lblSoftDocName.Size = new System.Drawing.Size(68, 25);
+            this.lblSoftDocName.TabIndex = 0;
+            this.lblSoftDocName.Text = "Name";
+            // 
+            // txtSoftDocName
+            // 
+            this.txtSoftDocName.Location = new System.Drawing.Point(123, 83);
+            this.txtSoftDocName.Name = "txtSoftDocName";
+            this.txtSoftDocName.Size = new System.Drawing.Size(185, 22);
+            this.txtSoftDocName.TabIndex = 1;
+            // 
+            // lblSoftDocDate
+            // 
+            this.lblSoftDocDate.AutoSize = true;
+            this.lblSoftDocDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftDocDate.Location = new System.Drawing.Point(353, 80);
+            this.lblSoftDocDate.Name = "lblSoftDocDate";
+            this.lblSoftDocDate.Size = new System.Drawing.Size(57, 25);
+            this.lblSoftDocDate.TabIndex = 2;
+            this.lblSoftDocDate.Text = "Date";
+            // 
+            // txtSoftDocDate
+            // 
+            this.txtSoftDocDate.Location = new System.Drawing.Point(416, 85);
+            this.txtSoftDocDate.Name = "txtSoftDocDate";
+            this.txtSoftDocDate.Size = new System.Drawing.Size(109, 22);
+            this.txtSoftDocDate.TabIndex = 3;
+            // 
+            // lblSoftDocRevision
+            // 
+            this.lblSoftDocRevision.AutoSize = true;
+            this.lblSoftDocRevision.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftDocRevision.Location = new System.Drawing.Point(572, 80);
+            this.lblSoftDocRevision.Name = "lblSoftDocRevision";
+            this.lblSoftDocRevision.Size = new System.Drawing.Size(94, 25);
+            this.lblSoftDocRevision.TabIndex = 4;
+            this.lblSoftDocRevision.Text = "Revision";
+            // 
+            // txtSoftDocRevision
+            // 
+            this.txtSoftDocRevision.Location = new System.Drawing.Point(672, 84);
+            this.txtSoftDocRevision.Name = "txtSoftDocRevision";
+            this.txtSoftDocRevision.Size = new System.Drawing.Size(147, 22);
+            this.txtSoftDocRevision.TabIndex = 5;
+            // 
+            // lblSoftDocLocation
+            // 
+            this.lblSoftDocLocation.AutoSize = true;
+            this.lblSoftDocLocation.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftDocLocation.Location = new System.Drawing.Point(851, 81);
+            this.lblSoftDocLocation.Name = "lblSoftDocLocation";
+            this.lblSoftDocLocation.Size = new System.Drawing.Size(94, 25);
+            this.lblSoftDocLocation.TabIndex = 6;
+            this.lblSoftDocLocation.Text = "Location";
+            // 
+            // txtSoftDocLocation
+            // 
+            this.txtSoftDocLocation.Location = new System.Drawing.Point(951, 84);
+            this.txtSoftDocLocation.Name = "txtSoftDocLocation";
+            this.txtSoftDocLocation.Size = new System.Drawing.Size(177, 22);
+            this.txtSoftDocLocation.TabIndex = 7;
+            // 
+            // lblSoftDocDescription
+            // 
+            this.lblSoftDocDescription.AutoSize = true;
+            this.lblSoftDocDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftDocDescription.Location = new System.Drawing.Point(54, 151);
+            this.lblSoftDocDescription.Name = "lblSoftDocDescription";
+            this.lblSoftDocDescription.Size = new System.Drawing.Size(120, 25);
+            this.lblSoftDocDescription.TabIndex = 8;
+            this.lblSoftDocDescription.Text = "Description";
+            // 
+            // txtSoftDocDescription
+            // 
+            this.txtSoftDocDescription.Location = new System.Drawing.Point(194, 153);
+            this.txtSoftDocDescription.Multiline = true;
+            this.txtSoftDocDescription.Name = "txtSoftDocDescription";
+            this.txtSoftDocDescription.Size = new System.Drawing.Size(830, 146);
+            this.txtSoftDocDescription.TabIndex = 9;
+            // 
+            // lblSoftDoc
+            // 
+            this.lblSoftDoc.AutoSize = true;
+            this.lblSoftDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftDoc.Location = new System.Drawing.Point(410, 26);
+            this.lblSoftDoc.Name = "lblSoftDoc";
+            this.lblSoftDoc.Size = new System.Drawing.Size(348, 32);
+            this.lblSoftDoc.TabIndex = 10;
+            this.lblSoftDoc.Text = "Software Documentation";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(336, 343);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(121, 53);
+            this.btnSubmit.TabIndex = 11;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(698, 343);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(121, 53);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // SoftwareDocForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1238, 427);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.lblSoftDoc);
+            this.Controls.Add(this.txtSoftDocDescription);
+            this.Controls.Add(this.lblSoftDocDescription);
+            this.Controls.Add(this.txtSoftDocLocation);
+            this.Controls.Add(this.lblSoftDocLocation);
+            this.Controls.Add(this.txtSoftDocRevision);
+            this.Controls.Add(this.lblSoftDocRevision);
+            this.Controls.Add(this.txtSoftDocDate);
+            this.Controls.Add(this.lblSoftDocDate);
+            this.Controls.Add(this.txtSoftDocName);
+            this.Controls.Add(this.lblSoftDocName);
+            this.Name = "SoftwareDocForm";
+            this.Text = "SoftwareDocForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSoftDocName;
+        private System.Windows.Forms.TextBox txtSoftDocName;
+        private System.Windows.Forms.Label lblSoftDocDate;
+        private System.Windows.Forms.TextBox txtSoftDocDate;
+        private System.Windows.Forms.Label lblSoftDocRevision;
+        private System.Windows.Forms.TextBox txtSoftDocRevision;
+        private System.Windows.Forms.Label lblSoftDocLocation;
+        private System.Windows.Forms.TextBox txtSoftDocLocation;
+        private System.Windows.Forms.Label lblSoftDocDescription;
+        private System.Windows.Forms.TextBox txtSoftDocDescription;
+        private System.Windows.Forms.Label lblSoftDoc;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
