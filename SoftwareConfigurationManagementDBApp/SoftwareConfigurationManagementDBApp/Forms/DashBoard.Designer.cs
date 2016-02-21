803dafe4e944ffd51a67ebc2bd852026f488582b
jreis491 <jrei@gru.edu>
2016-02-21 13:40:22 -0500

    Added the Dashboard

---
+++ SoftwareConfigurationManagementDBApp/SoftwareConfigurationManagementDBApp/Forms/DashBoard.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace SoftwareConfigurationManagementDBApp.Forms
+{
+    partial class DashBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogOut = new System.Windows.Forms.Button();
+            this.viewlb = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnViewAttr = new System.Windows.Forms.Button();
+            this.btnDeleteAttr = new System.Windows.Forms.Button();
+            this.btnEditAttr = new System.Windows.Forms.Button();
+            this.btnAddAttr = new System.Windows.Forms.Button();
+            this.btnEditUser = new System.Windows.Forms.Button();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.btnDeleteSoft = new System.Windows.Forms.Button();
+            this.btnEditSoftw = new System.Windows.Forms.Button();
+            this.btnAddSoftw = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLogOut
+            // 
+            this.btnLogOut.Location = new System.Drawing.Point(104, 563);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(86, 31);
+            this.btnLogOut.TabIndex = 38;
+            this.btnLogOut.Text = "Log Out";
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            // 
+            // viewlb
+            // 
+            this.viewlb.AutoSize = true;
+            this.viewlb.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewlb.Location = new System.Drawing.Point(99, 102);
+            this.viewlb.Name = "viewlb";
+            this.viewlb.Size = new System.Drawing.Size(63, 26);
+            this.viewlb.TabIndex = 37;
+            this.viewlb.Text = "View";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(57, 147);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 21);
+            this.comboBox1.TabIndex = 36;
+            // 
+            // btnViewAttr
+            // 
+            this.btnViewAttr.Location = new System.Drawing.Point(185, 372);
+            this.btnViewAttr.Name = "btnViewAttr";
+            this.btnViewAttr.Size = new System.Drawing.Size(86, 31);
+            this.btnViewAttr.TabIndex = 35;
+            this.btnViewAttr.Text = "View Attributes";
+            this.btnViewAttr.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteAttr
+            // 
+            this.btnDeleteAttr.Location = new System.Drawing.Point(185, 305);
+            this.btnDeleteAttr.Name = "btnDeleteAttr";
+            this.btnDeleteAttr.Size = new System.Drawing.Size(86, 46);
+            this.btnDeleteAttr.TabIndex = 34;
+            this.btnDeleteAttr.Text = "Delete Attribute";
+            this.btnDeleteAttr.UseVisualStyleBackColor = true;
+            // 
+            // btnEditAttr
+            // 
+            this.btnEditAttr.Location = new System.Drawing.Point(185, 258);
+            this.btnEditAttr.Name = "btnEditAttr";
+            this.btnEditAttr.Size = new System.Drawing.Size(86, 31);
+            this.btnEditAttr.TabIndex = 33;
+            this.btnEditAttr.Text = "Edit Attribute";
+            this.btnEditAttr.UseVisualStyleBackColor = true;
+            // 
+            // btnAddAttr
+            // 
+            this.btnAddAttr.Location = new System.Drawing.Point(185, 208);
+            this.btnAddAttr.Name = "btnAddAttr";
+            this.btnAddAttr.Size = new System.Drawing.Size(86, 31);
+            this.btnAddAttr.TabIndex = 32;
+            this.btnAddAttr.Text = "Add Attribute";
+            this.btnAddAttr.UseVisualStyleBackColor = true;
+            // 
+            // btnEditUser
+            // 
+            this.btnEditUser.Location = new System.Drawing.Point(30, 426);
+            this.btnEditUser.Name = "btnEditUser";
+            this.btnEditUser.Size = new System.Drawing.Size(86, 31);
+            this.btnEditUser.TabIndex = 31;
+            this.btnEditUser.Text = "Edit User";
+            this.btnEditUser.UseVisualStyleBackColor = true;
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(30, 372);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(86, 31);
+            this.btnAddUser.TabIndex = 30;
+            this.btnAddUser.Text = "Add User";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteSoft
+            // 
+            this.btnDeleteSoft.Location = new System.Drawing.Point(30, 305);
+            this.btnDeleteSoft.Name = "btnDeleteSoft";
+            this.btnDeleteSoft.Size = new System.Drawing.Size(86, 46);
+            this.btnDeleteSoft.TabIndex = 29;
+            this.btnDeleteSoft.Text = "Delete Software";
+            this.btnDeleteSoft.UseVisualStyleBackColor = true;
+            // 
+            // btnEditSoftw
+            // 
+            this.btnEditSoftw.Location = new System.Drawing.Point(30, 258);
+            this.btnEditSoftw.Name = "btnEditSoftw";
+            this.btnEditSoftw.Size = new System.Drawing.Size(86, 31);
+            this.btnEditSoftw.TabIndex = 28;
+            this.btnEditSoftw.Text = "Edit Software";
+            this.btnEditSoftw.UseVisualStyleBackColor = true;
+            // 
+            // btnAddSoftw
+            // 
+            this.btnAddSoftw.Location = new System.Drawing.Point(30, 208);
+            this.btnAddSoftw.Name = "btnAddSoftw";
+            this.btnAddSoftw.Size = new System.Drawing.Size(86, 31);
+            this.btnAddSoftw.TabIndex = 27;
+            this.btnAddSoftw.Text = "Add Software";
+            this.btnAddSoftw.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(337, 84);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(766, 494);
+            this.dataGridView1.TabIndex = 39;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(64, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 36);
+            this.label1.TabIndex = 40;
+            this.label1.Text = "DashBoard";
+            // 
+            // DashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1126, 614);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLogOut);
+            this.Controls.Add(this.viewlb);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnViewAttr);
+            this.Controls.Add(this.btnDeleteAttr);
+            this.Controls.Add(this.btnEditAttr);
+            this.Controls.Add(this.btnAddAttr);
+            this.Controls.Add(this.btnEditUser);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.btnDeleteSoft);
+            this.Controls.Add(this.btnEditSoftw);
+            this.Controls.Add(this.btnAddSoftw);
+            this.Name = "DashBoard";
+            this.Text = "DashBoard";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogOut;
+        private System.Windows.Forms.Label viewlb;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnViewAttr;
+        private System.Windows.Forms.Button btnDeleteAttr;
+        private System.Windows.Forms.Button btnEditAttr;
+        private System.Windows.Forms.Button btnAddAttr;
+        private System.Windows.Forms.Button btnEditUser;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.Button btnDeleteSoft;
+        private System.Windows.Forms.Button btnEditSoftw;
+        private System.Windows.Forms.Button btnAddSoftw;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
