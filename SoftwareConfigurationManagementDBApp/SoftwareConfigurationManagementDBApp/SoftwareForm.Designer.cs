2010708c6dd3ac0ad3a42cd75920b4dcaba085cb
cevans <CEVANS26@gru.edu>
2016-02-20 20:33:29 -0500

    Added SoftwareForm -Brent

---
+++ SoftwareConfigurationManagementDBApp/SoftwareConfigurationManagementDBApp/SoftwareForm.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace SoftwareConfigurationManagementDBApp
+{
+    partial class SoftwareForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSoftwareName = new System.Windows.Forms.Label();
+            this.txtSoftwareName = new System.Windows.Forms.TextBox();
+            this.lblSoftwareDescription = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblOwner = new System.Windows.Forms.Label();
+            this.txtOwner = new System.Windows.Forms.TextBox();
+            this.lblResponsibleEngineer = new System.Windows.Forms.Label();
+            this.txtResponsibleEngineer = new System.Windows.Forms.TextBox();
+            this.lblDesignAuthority = new System.Windows.Forms.Label();
+            this.txtDesignAuthority = new System.Windows.Forms.TextBox();
+            this.lblManager = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.cmbClass = new System.Windows.Forms.ComboBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblSoftwareSystem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblSoftwareName
+            // 
+            this.lblSoftwareName.AutoSize = true;
+            this.lblSoftwareName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftwareName.Location = new System.Drawing.Point(183, 106);
+            this.lblSoftwareName.Name = "lblSoftwareName";
+            this.lblSoftwareName.Size = new System.Drawing.Size(68, 25);
+            this.lblSoftwareName.TabIndex = 0;
+            this.lblSoftwareName.Text = "Name";
+            // 
+            // txtSoftwareName
+            // 
+            this.txtSoftwareName.Location = new System.Drawing.Point(258, 108);
+            this.txtSoftwareName.Name = "txtSoftwareName";
+            this.txtSoftwareName.Size = new System.Drawing.Size(199, 22);
+            this.txtSoftwareName.TabIndex = 1;
+            // 
+            // lblSoftwareDescription
+            // 
+            this.lblSoftwareDescription.AutoSize = true;
+            this.lblSoftwareDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftwareDescription.Location = new System.Drawing.Point(502, 108);
+            this.lblSoftwareDescription.Name = "lblSoftwareDescription";
+            this.lblSoftwareDescription.Size = new System.Drawing.Size(120, 25);
+            this.lblSoftwareDescription.TabIndex = 2;
+            this.lblSoftwareDescription.Text = "Description";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(629, 110);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(504, 120);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblOwner
+            // 
+            this.lblOwner.AutoSize = true;
+            this.lblOwner.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOwner.Location = new System.Drawing.Point(176, 155);
+            this.lblOwner.Name = "lblOwner";
+            this.lblOwner.Size = new System.Drawing.Size(75, 25);
+            this.lblOwner.TabIndex = 4;
+            this.lblOwner.Text = "Owner";
+            // 
+            // txtOwner
+            // 
+            this.txtOwner.Location = new System.Drawing.Point(258, 155);
+            this.txtOwner.Name = "txtOwner";
+            this.txtOwner.Size = new System.Drawing.Size(199, 22);
+            this.txtOwner.TabIndex = 5;
+            // 
+            // lblResponsibleEngineer
+            // 
+            this.lblResponsibleEngineer.AutoSize = true;
+            this.lblResponsibleEngineer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResponsibleEngineer.Location = new System.Drawing.Point(29, 237);
+            this.lblResponsibleEngineer.Name = "lblResponsibleEngineer";
+            this.lblResponsibleEngineer.Size = new System.Drawing.Size(222, 25);
+            this.lblResponsibleEngineer.TabIndex = 6;
+            this.lblResponsibleEngineer.Text = "Responsible Engineer";
+            // 
+            // txtResponsibleEngineer
+            // 
+            this.txtResponsibleEngineer.Location = new System.Drawing.Point(258, 239);
+            this.txtResponsibleEngineer.Name = "txtResponsibleEngineer";
+            this.txtResponsibleEngineer.Size = new System.Drawing.Size(199, 22);
+            this.txtResponsibleEngineer.TabIndex = 7;
+            // 
+            // lblDesignAuthority
+            // 
+            this.lblDesignAuthority.AutoSize = true;
+            this.lblDesignAuthority.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesignAuthority.Location = new System.Drawing.Point(80, 195);
+            this.lblDesignAuthority.Name = "lblDesignAuthority";
+            this.lblDesignAuthority.Size = new System.Drawing.Size(171, 25);
+            this.lblDesignAuthority.TabIndex = 8;
+            this.lblDesignAuthority.Text = "Design Authority";
+            // 
+            // txtDesignAuthority
+            // 
+            this.txtDesignAuthority.Location = new System.Drawing.Point(258, 197);
+            this.txtDesignAuthority.Name = "txtDesignAuthority";
+            this.txtDesignAuthority.Size = new System.Drawing.Size(199, 22);
+            this.txtDesignAuthority.TabIndex = 9;
+            // 
+            // lblManager
+            // 
+            this.lblManager.AutoSize = true;
+            this.lblManager.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblManager.Location = new System.Drawing.Point(154, 277);
+            this.lblManager.Name = "lblManager";
+            this.lblManager.Size = new System.Drawing.Size(97, 25);
+            this.lblManager.TabIndex = 10;
+            this.lblManager.Text = "Manager";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(258, 279);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(199, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // lblClass
+            // 
+            this.lblClass.AutoSize = true;
+            this.lblClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClass.Location = new System.Drawing.Point(486, 257);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(136, 25);
+            this.lblClass.TabIndex = 12;
+            this.lblClass.Text = "Classfication";
+            // 
+            // cmbClass
+            // 
+            this.cmbClass.FormattingEnabled = true;
+            this.cmbClass.Location = new System.Drawing.Point(629, 256);
+            this.cmbClass.Name = "cmbClass";
+            this.cmbClass.Size = new System.Drawing.Size(110, 24);
+            this.cmbClass.TabIndex = 13;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(316, 349);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(120, 53);
+            this.btnSubmit.TabIndex = 14;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.Location = new System.Drawing.Point(677, 349);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(120, 53);
+            this.btnBack.TabIndex = 15;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // lblSoftwareSystem
+            // 
+            this.lblSoftwareSystem.AutoSize = true;
+            this.lblSoftwareSystem.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSoftwareSystem.Location = new System.Drawing.Point(457, 37);
+            this.lblSoftwareSystem.Name = "lblSoftwareSystem";
+            this.lblSoftwareSystem.Size = new System.Drawing.Size(243, 32);
+            this.lblSoftwareSystem.TabIndex = 16;
+            this.lblSoftwareSystem.Text = "Software System";
+            // 
+            // SoftwareForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 481);
+            this.Controls.Add(this.lblSoftwareSystem);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.cmbClass);
+            this.Controls.Add(this.lblClass);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lblManager);
+            this.Controls.Add(this.txtDesignAuthority);
+            this.Controls.Add(this.lblDesignAuthority);
+            this.Controls.Add(this.txtResponsibleEngineer);
+            this.Controls.Add(this.lblResponsibleEngineer);
+            this.Controls.Add(this.txtOwner);
+            this.Controls.Add(this.lblOwner);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblSoftwareDescription);
+            this.Controls.Add(this.txtSoftwareName);
+            this.Controls.Add(this.lblSoftwareName);
+            this.Name = "SoftwareForm";
+            this.Text = "Software System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSoftwareName;
+        private System.Windows.Forms.TextBox txtSoftwareName;
+        private System.Windows.Forms.Label lblSoftwareDescription;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblOwner;
+        private System.Windows.Forms.TextBox txtOwner;
+        private System.Windows.Forms.Label lblResponsibleEngineer;
+        private System.Windows.Forms.TextBox txtResponsibleEngineer;
+        private System.Windows.Forms.Label lblDesignAuthority;
+        private System.Windows.Forms.TextBox txtDesignAuthority;
+        private System.Windows.Forms.Label lblManager;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.ComboBox cmbClass;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblSoftwareSystem;
+    }
+}
