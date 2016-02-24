cd7d0999b98a92cc99be1688e2f4d021ca6645f1
cevans <CEVANS26@gru.edu>
2016-02-24 14:33:14 -0500

    Added Attributes Entity with child classes: SoftwareDoc, CI, CIDoc

---
+++ SoftwareConfigurationManagementDBApp/SoftwareConfigurationManagementDBApp/Data Objects/Attributes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace SoftwareConfigurationManagementDBApp
+{
+    /// <summary>
+    ///     Parent class Attributes is for storing
+    ///     data into the database. Child classes:
+    ///     SoftwareDoc, CI, CIDoc
+    /// </summary>
+    
+    [DataContract]
+     public class Attributes
+    {
+        // Data Types // ============================ //
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Revision { get; set; }
+
+        [DataMember]
+        public string Date { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        [DataMember]
+        public string Location { get; set; }
+        // ========================================== //
+    }
+
+    /// <summary>
+    ///     Child Class
+    /// </summary>
+    public class SoftwareDoc : Attributes {}
+
+    public class CI : Attributes {}
+
+    public class CIDoc : Attributes {}
+}
