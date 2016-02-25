85fa3515e547b84764a2b04c88d70ef57a752ee8
jreis491 <jrei@gru.edu>
2016-02-24 22:41:19 -0500

    edited the ci doc form

--- SoftwareConfigurationManagementDBApp/SoftwareConfigurationManagementDBApp/Forms/CIDoc.Designer.cs
+++ SoftwareConfigurationManagementDBApp/SoftwareConfigurationManagementDBApp/Forms/CIDoc.Designer.cs
@@ -31,13 +31,11 @@
             this.btnSubmitCIDoc = new System.Windows.Forms.Button();
             this.chkboxNoDocCI = new System.Windows.Forms.CheckBox();
             this.txtCIInfoCI = new System.Windows.Forms.TextBox();
-            this.txtCIDocNmCI = new System.Windows.Forms.TextBox();
-            this.txtCisCI = new System.Windows.Forms.TextBox();
+            this.txtCIDocNmCIDoc = new System.Windows.Forms.TextBox();
             this.txtCIIndexCI = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -45,7 +43,7 @@
             // btnSubmitCIDoc
             // 
             this.btnSubmitCIDoc.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSubmitCIDoc.Location = new System.Drawing.Point(283, 396);
+            this.btnSubmitCIDoc.Location = new System.Drawing.Point(281, 332);
             this.btnSubmitCIDoc.Name = "btnSubmitCIDoc";
             this.btnSubmitCIDoc.Size = new System.Drawing.Size(85, 31);
             this.btnSubmitCIDoc.TabIndex = 36;
@@ -55,7 +53,7 @@
             // chkboxNoDocCI
             // 
             this.chkboxNoDocCI.AutoSize = true;
-            this.chkboxNoDocCI.Location = new System.Drawing.Point(255, 374);
+            this.chkboxNoDocCI.Location = new System.Drawing.Point(253, 310);
             this.chkboxNoDocCI.Name = "chkboxNoDocCI";
             this.chkboxNoDocCI.Size = new System.Drawing.Size(15, 14);
             this.chkboxNoDocCI.TabIndex = 35;
@@ -63,25 +61,18 @@
             // 
             // txtCIInfoCI
             // 
-            this.txtCIInfoCI.Location = new System.Drawing.Point(179, 250);
+            this.txtCIInfoCI.Location = new System.Drawing.Point(177, 186);
             this.txtCIInfoCI.Multiline = true;
             this.txtCIInfoCI.Name = "txtCIInfoCI";
             this.txtCIInfoCI.Size = new System.Drawing.Size(397, 99);
             this.txtCIInfoCI.TabIndex = 34;
             // 
-            // txtCIDocNmCI
+            // txtCIDocNmCIDoc
             // 
-            this.txtCIDocNmCI.Location = new System.Drawing.Point(158, 210);
-            this.txtCIDocNmCI.Name = "txtCIDocNmCI";
-            this.txtCIDocNmCI.Size = new System.Drawing.Size(210, 20);
-            this.txtCIDocNmCI.TabIndex = 33;
-            // 
-            // txtCisCI
-            // 
-            this.txtCisCI.Location = new System.Drawing.Point(115, 157);
-            this.txtCisCI.Name = "txtCisCI";
-            this.txtCisCI.Size = new System.Drawing.Size(166, 20);
-            this.txtCisCI.TabIndex = 32;
+            this.txtCIDocNmCIDoc.Location = new System.Drawing.Point(156, 146);
+            this.txtCIDocNmCIDoc.Name = "txtCIDocNmCIDoc";
+            this.txtCIDocNmCIDoc.Size = new System.Drawing.Size(210, 20);
+            this.txtCIDocNmCIDoc.TabIndex = 33;
             // 
             // txtCIIndexCI
             // 
@@ -94,7 +85,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(53, 369);
+            this.label6.Location = new System.Drawing.Point(51, 305);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(206, 19);
             this.label6.TabIndex = 30;
@@ -104,7 +95,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(53, 250);
+            this.label5.Location = new System.Drawing.Point(51, 186);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(120, 19);
             this.label5.TabIndex = 29;
@@ -114,21 +105,11 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(53, 209);
+            this.label4.Location = new System.Drawing.Point(51, 145);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(138, 19);
+            this.label4.Size = new System.Drawing.Size(99, 19);
             this.label4.TabIndex = 28;
-            this.label4.Text = "Software Doc Name:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(53, 156);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 19);
-            this.label3.TabIndex = 27;
-            this.label3.Text = "CIs Doc:";
+            this.label4.Text = "CI Doc Name:";
             // 
             // label2
             // 
@@ -154,17 +135,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(543, 545);
+            this.ClientSize = new System.Drawing.Size(606, 386);
             this.Controls.Add(this.btnSubmitCIDoc);
             this.Controls.Add(this.chkboxNoDocCI);
             this.Controls.Add(this.txtCIInfoCI);
-            this.Controls.Add(this.txtCIDocNmCI);
-            this.Controls.Add(this.txtCisCI);
+            this.Controls.Add(this.txtCIDocNmCIDoc);
             this.Controls.Add(this.txtCIIndexCI);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "CIDoc";
@@ -179,13 +158,11 @@
         private System.Windows.Forms.Button btnSubmitCIDoc;
         private System.Windows.Forms.CheckBox chkboxNoDocCI;
         private System.Windows.Forms.TextBox txtCIInfoCI;
-        private System.Windows.Forms.TextBox txtCIDocNmCI;
-        private System.Windows.Forms.TextBox txtCisCI;
+        private System.Windows.Forms.TextBox txtCIDocNmCIDoc;
         private System.Windows.Forms.TextBox txtCIIndexCI;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
     }
