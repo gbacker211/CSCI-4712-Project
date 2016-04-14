91d4e91399983a6a2d6c7f068a203adf05510cfe
jreis491 <jrei@gru.edu>
2016-04-14 17:55:06 -0400

    added the pathway form

---
+++ SoftwareConfigurationManagementDBApp/SoftwareConfigurationManagementDBApp/Forms/Pathway.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SoftwareConfigurationManagementDBApp.Forms
+{
+    partial class Pathway
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPathway = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please specify the pathway";
+            // 
+            // txtPathway
+            // 
+            this.txtPathway.Location = new System.Drawing.Point(44, 80);
+            this.txtPathway.Name = "txtPathway";
+            this.txtPathway.Size = new System.Drawing.Size(225, 20);
+            this.txtPathway.TabIndex = 1;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(93, 133);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(96, 36);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "connect";
+            this.btnConnect.UseMnemonic = false;
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // Pathway
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(310, 220);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPathway);
+            this.Controls.Add(this.label1);
+            this.Name = "Pathway";
+            this.Text = "Pathway";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPathway;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
